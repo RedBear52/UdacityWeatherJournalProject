a90c7fbaf90aeb13086f0e0367e2885653fc8bef
ryan-spearman <ryanspearman@gmail.com>
2022-06-29 16:59:14 -0500

    Initial Commit  |  Starter(Comment Only Code) + 1st Attempt at Setting UP Server-Side JS File

---
+++ commentsOnlyJS/server.js
@@ -0,0 +1,37 @@
+// Setup empty JS object to act as endpoint for all routes
+const projectData = {}
+// Express to run server and routes
+const express = require('express')
+// Start up an instance of app
+const app = express()
+/* Dependencies */
+const bodyParser = require('body-parser')
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+// Cors for cross origin allowance
+const cors = require('cors')
+// Initialize the main project folder
+app.use(express.static('website'))
+// Spin up the server
+const port = 3000
+const server = app.listen(port, () => {
+    console.log("Server is listening on:")
+    console.log(`Port: ${port}`)
+})
+// Callback to debug
+app.get('/', (req, res) => {
+    res.send('Howdy Multiverse!')
+})
+// Initialize all route with a callback function
+app.get('/all', sendProjectData)
+// Callback function to complete GET '/all'
+const sendProjectData = (req, res) => {
+    res.send(projectData)
+}
+// Post Route
+app.post('/', updateData) 
+const updateData = (req, res) => {
+    res.send('POST receiveed')
+}
